d8c6711aba4109ebb644e6f540a9ab98f415a5c1
Adam Dangoor <adamdangoor@gmail.com>
2019-11-23 11:08:57 +0000

    Add stubs for add target tests

---
+++ tests/test_add_target.py
@@ -0,0 +1,42 @@
+"""
+XXX
+"""
+
+import io
+
+import yaml
+from click.testing import CliRunner
+from mock_vws.database import VuforiaDatabase
+from vws import VWS
+
+from vws_cli import vws_group
+
+def test_add_target():
+    pass
+
+def test_custom_metadata():
+    pass
+
+def test_custom_active_flag():
+    pass
+
+def test_bad_image():
+    pass
+
+def test_fail():
+    pass
+
+def test_metadata_too_large():
+    pass
+
+def test_image_too_large():
+    pass
+
+def test_target_name_exist():
+    pass
+
+def test_project_inactive():
+    pass
+
+def test_unknown_vws_error():
+    pass
