dd6a0e29c10660ede79168f9214670f102aae883
Adam Dangoor <adamdangoor@gmail.com>
2019-11-29 16:20:04 +0000

    Bump requirements

--- src/vws_cli/options/targets.py
+++ src/vws_cli/options/targets.py
@@ -11,8 +11,8 @@ def target_id_option(command: Callable[..., None]) -> Callable[..., None]:
     """
     An option decorator for choosing a target ID.
     """
-    click_option_function: Callable[
-        [Callable[..., None]], Callable[..., None]] = click.option(
+    click_option_function: Callable[[Callable[..., None]], Callable[
+        ..., None]] = click.option(
             '--target-id',
             type=str,
             help='The ID of a target in the Vuforia database.',
