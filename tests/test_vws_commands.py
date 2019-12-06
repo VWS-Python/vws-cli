7dd13fa9a731c115094dab140b42a86554aa2de3
Adam Dangoor <adamdangoor@gmail.com>
2019-12-06 14:10:44 +0000

    Remove duplicate test

--- tests/test_vws_commands.py
+++ tests/test_vws_commands.py
@@ -19,30 +19,6 @@ from vws import VWS, CloudRecoService
 from vws_cli import vws_group
 
 
-def test_target_id_does_not_exist(mock_database: VuforiaDatabase) -> None:
-    """
-    Commands which take a target ID show an error if that does not map to a
-    target in the database.
-    """
-    runner = CliRunner(mix_stderr=False)
-    for command_name, command in vws_group.commands.items():
-        if 'target_id' in [option.name for option in command.params]:
-            args = [
-                command_name,
-                '--target-id',
-                'x/1',
-                '--server-access-key',
-                mock_database.server_access_key,
-                '--server-secret-key',
-                mock_database.server_secret_key,
-            ]
-            result = runner.invoke(vws_group, args, catch_exceptions=False)
-            assert result.exit_code == 1
-            expected_stderr = 'Target "x/1" does not exist.\n'
-            assert result.stderr == expected_stderr
-            assert result.stdout == ''
-
-
 def test_get_database_summary_report(
     mock_database: VuforiaDatabase,
     vws_client: VWS,
--- tests/test_vws_errors.py
+++ tests/test_vws_errors.py
@@ -2,15 +2,10 @@
 Tests for how errors from VWS are handled by the CLI.
 """
 
-import io
-from pathlib import Path
-
 from click.testing import CliRunner
 from mock_vws.database import VuforiaDatabase
-from vws import VWS
 
 from vws_cli import vws_group
-import uuid
 
 
 def test_target_id_does_not_exist(mock_database: VuforiaDatabase) -> None:
