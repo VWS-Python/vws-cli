7705d6149fc9c8aeb5ee494cc726d1239cf0c073
Adam Dangoor <adamdangoor@gmail.com>
2019-12-22 22:33:05 +0000

    Add test for new command's help text

--- tests/test_help.py
+++ tests/test_help.py
@@ -10,6 +10,7 @@ import pytest
 from click.testing import CliRunner
 
 from vws_cli import vws_group
+from vws_cli.query import vuforia_cloud_reco
 
 _SUBCOMMANDS = [[item] for item in vws_group.commands.keys()]
 _BASE_COMMAND: List[List[str]] = [[]]
@@ -21,19 +22,48 @@ _COMMANDS = _BASE_COMMAND + _SUBCOMMANDS
     _COMMANDS,
     ids=[str(cmd) for cmd in _COMMANDS],
 )
-def test_help(command: List[str]) -> None:
+def test_vws_command_help(command: List[str]) -> None:
     """
     Expected help text is shown for ``vws`` commands.
 
     This help text is defined in files.
-    To update these files, run the command
-    ``bash admin/update_cli_tests.sh``.
+    To update these files, run the command ``bash admin/update_cli_tests.sh``.
     """
     runner = CliRunner()
     arguments = command + ['--help']
     result = runner.invoke(vws_group, arguments, catch_exceptions=False)
     assert result.exit_code == 0
     help_output_filename = '-'.join(['vws'] + command) + '.txt'
+    help_outputs_dir = Path(__file__).parent / 'help_outputs'
+    expected_help_file = help_outputs_dir / help_output_filename
+    try:
+        expected_help = expected_help_file.read_text()
+        assert result.output == expected_help
+    except (AssertionError, FileNotFoundError):  # pragma: no cover
+        if os.getenv('FIX_CLI_TESTS') == '1':
+            help_outputs_dir.mkdir(exist_ok=True)
+            expected_help_file.touch()
+            expected_help_file.write_text(result.output)
+        else:
+            raise
+
+
+def test_query_help() -> None:
+    """
+    Expected help text is shown for the ``vuforia-cloud-reco`` command.
+
+    This help text is defined in files.
+    To update these files, run the command ``bash admin/update_cli_tests.sh``.
+    """
+    runner = CliRunner()
+    arguments = ['--help']
+    result = runner.invoke(
+        vuforia_cloud_reco,
+        arguments,
+        catch_exceptions=False,
+    )
+    assert result.exit_code == 0
+    help_output_filename = 'vuforia_cloud_reco.txt'
     help_outputs_dir = Path(__file__).parent / 'help_outputs'
     expected_help_file = help_outputs_dir / help_output_filename
     try:
