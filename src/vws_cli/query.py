af9dd0872efdcf554a54e2acfe5a398f010d3333
Adam Dangoor <adamdangoor@gmail.com>
2019-12-22 23:10:50 +0000

    Add asserts

--- src/vws_cli/query.py
+++ src/vws_cli/query.py
@@ -17,3 +17,5 @@ def vuforia_cloud_reco(client_access_key: str, client_secret_key: str) -> None:
     """
     Make a request to the Vuforia Cloud Recognition Service API.
     """
+    assert client_access_key
+    assert client_secret_key
