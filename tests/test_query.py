786a49bf4b3243b7dc5901c6ec9307d2fc75de3e
Adam Dangoor <adamdangoor@gmail.com>
2019-12-24 22:22:57 +0000

    Add test for custom base vwq url

--- tests/test_query.py
+++ tests/test_query.py
@@ -10,6 +10,7 @@ from typing import List
 
 import yaml
 from click.testing import CliRunner
+from mock_vws import MockVWS
 from mock_vws.database import VuforiaDatabase
 from vws import VWS
 
@@ -610,3 +611,51 @@ class TestIncludeTargetData:
             'top, none, all)'
         )
         assert expected_stderr in result.stderr
+
+
+def test_base_vwq_url(high_quality_image: io.BytesIO, tmp_path: Path) -> None:
+    """
+    It is possible to use query a target to a database under a custom VWQ
+    URL.
+    """
+    runner = CliRunner(mix_stderr=False)
+    base_vwq_url = 'http://example.com'
+    new_file = tmp_path / uuid.uuid4().hex
+    image_data = high_quality_image.getvalue()
+    new_file.write_bytes(data=image_data)
+    with MockVWS(base_vwq_url=base_vwq_url) as mock:
+        mock_database = VuforiaDatabase()
+        mock.add_database(database=mock_database)
+        vws_client = VWS(
+            server_access_key=mock_database.server_access_key,
+            server_secret_key=mock_database.server_secret_key,
+        )
+
+        target_id = vws_client.add_target(
+            name='x',
+            width=1,
+            image=high_quality_image,
+            active_flag=True,
+            application_metadata=None,
+        )
+
+        vws_client.wait_for_target_processed(target_id=target_id)
+
+        commands = [
+            str(new_file),
+            '--client-access-key',
+            mock_database.client_access_key,
+            '--client-secret-key',
+            mock_database.client_secret_key,
+            '--base-vwq-url',
+            base_vwq_url,
+        ]
+        result = runner.invoke(
+            vuforia_cloud_reco,
+            commands,
+            catch_exceptions=False,
+        )
+        assert result.exit_code == 0
+
+    [match] = yaml.load(result.stdout, Loader=yaml.FullLoader)
+    assert match['target_id'] == target_id
