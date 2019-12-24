b1e2500020bb0ac676db1c7caa4318a9610f3911
Adam Dangoor <adamdangoor@gmail.com>
2019-12-24 11:48:57 +0000

    Make test pass

--- src/vws_cli/query.py
+++ src/vws_cli/query.py
@@ -34,10 +34,25 @@ def image_argument(command: Callable[..., None]) -> Callable[..., None]:
     return click_option_function(command)
 
 
+def max_num_results_option(
+    command: Callable[..., None],
+) -> Callable[..., None]:
+    """
+    An option decorator for choosing the maximum number of query results.
+    """
+    click_option_function = click.option(
+        '--max-num-results',
+        type=click.IntRange(min=1, max=50),
+        default=1,
+    )
+
+    return click_option_function(command)
+
 @click.command(name='vuforia-cloud-reco')
 @image_argument
 @client_access_key_option
 @client_secret_key_option
+@max_num_results_option
 # We set the ``version`` parameter because in PyInstaller binaries,
 # ``pkg_resources`` is not available.
 #
@@ -47,6 +62,7 @@ def vuforia_cloud_reco(
     image: Path,
     client_access_key: str,
     client_secret_key: str,
+    max_num_results: int,
 ) -> None:
     """
     Make a request to the Vuforia Cloud Recognition Service API.
@@ -55,6 +71,9 @@ def vuforia_cloud_reco(
         client_access_key=client_access_key,
         client_secret_key=client_secret_key,
     )
-    query_result = client.query(image=io.BytesIO(image.read_bytes()))
+    query_result = client.query(
+        image=io.BytesIO(image.read_bytes()),
+        max_num_results=max_num_results,
+    )
     yaml_list = yaml.dump(query_result)
     click.echo(yaml_list)
