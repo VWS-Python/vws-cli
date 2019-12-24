c2a32c63fff17e1743e167a4c1f03f3d0cc42fdd
Adam Dangoor <adamdangoor@gmail.com>
2019-12-24 12:41:55 +0000

    Add start of test for the default target data option

--- tests/test_query.py
+++ tests/test_query.py
@@ -360,3 +360,55 @@ class TestMaxNumResults:
             'valid range of 1 to 50.'
         )
         assert expected_stderr_substring in result.stderr
+
+
+class TestIncludeTargetData:
+    """
+    Tests for the ``--include-target-data`` option.
+    """
+
+    def test_default(
+        self,
+        vws_client: VWS,
+        high_quality_image: io.BytesIO,
+        tmp_path: Path,
+        mock_database: VuforiaDatabase,
+    ) -> None:
+        """
+        By default, target data is only returned in the top match.
+        """
+        runner = CliRunner(mix_stderr=False)
+        target_id = vws_client.add_target(
+            name=uuid.uuid4().hex,
+            width=1,
+            image=high_quality_image,
+            active_flag=True,
+            application_metadata=None,
+        )
+        target_id_2 = vws_client.add_target(
+            name=uuid.uuid4().hex,
+            width=1,
+            image=high_quality_image,
+            active_flag=True,
+            application_metadata=None,
+        )
+        vws_client.wait_for_target_processed(target_id=target_id)
+        vws_client.wait_for_target_processed(target_id=target_id_2)
+        new_file = tmp_path / uuid.uuid4().hex
+        image_data = high_quality_image.getvalue()
+        new_file.write_bytes(data=image_data)
+        commands = [
+            str(new_file),
+            '--max-num-results',
+            str(2),
+            '--client-access-key',
+            mock_database.client_access_key,
+            '--client-secret-key',
+            mock_database.client_secret_key,
+        ]
+        result = runner.invoke(
+            vuforia_cloud_reco,
+            commands,
+            catch_exceptions=False,
+        )
+        assert result.exit_code == 0
