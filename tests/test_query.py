eac0c1cd4a5773477087cd7ad7f75efa60d2df88
Adam Dangoor <adamdangoor@gmail.com>
2019-12-24 11:43:33 +0000

    Add test for custom max num results

--- tests/test_query.py
+++ tests/test_query.py
@@ -280,4 +280,49 @@ class TestMaxNumResults:
         """
         It is possible to set a custom ``--max-num-results``.
         """
+        runner = CliRunner(mix_stderr=False)
+        target_id = vws_client.add_target(
+            name=uuid.uuid4().hex,
+            width=1,
+            image=high_quality_image,
+            active_flag=True,
+            application_metadata=None,
+        )
+        target_id_2 = vws_client.add_target(
+            name=uuid.uuid4().hex,
+            width=1,
+            image=high_quality_image,
+            active_flag=True,
+            application_metadata=None,
+        )
+        target_id_3 = vws_client.add_target(
+            name=uuid.uuid4().hex,
+            width=1,
+            image=high_quality_image,
+            active_flag=True,
+            application_metadata=None,
+        )
+        vws_client.wait_for_target_processed(target_id=target_id)
+        vws_client.wait_for_target_processed(target_id=target_id_2)
+        vws_client.wait_for_target_processed(target_id=target_id_3)
 
+        new_file = tmp_path / uuid.uuid4().hex
+        image_data = high_quality_image.getvalue()
+        new_file.write_bytes(data=image_data)
+        commands = [
+            str(new_file),
+            '--max-num-results',
+            2,
+            '--client-access-key',
+            mock_database.client_access_key,
+            '--client-secret-key',
+            mock_database.client_secret_key,
+        ]
+        result = runner.invoke(
+            vuforia_cloud_reco,
+            commands,
+            catch_exceptions=False,
+        )
+        assert result.exit_code == 0
+        result_data = yaml.load(result.stdout, Loader=yaml.FullLoader)
+        assert len(result_data) == 2
