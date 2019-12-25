dd8eb1e1337ee61c8280247014e5792a28d45455
Adam Dangoor <adamdangoor@gmail.com>
2019-12-25 09:38:12 +0000

    Progress towards creating homebrew recipe

--- admin/homebrew.py
+++ admin/homebrew.py
@@ -1,13 +1,30 @@
-from pathlib import Path
+"""
+Tools for creating Homebrew recipes.
+"""
+
 import subprocess
+from pathlib import Path
+from typing import List
+
+
+def _get_dependencies(requirements_file: Path) -> List[str]:
+    """
+    Return requirements from a requirements file.
+
+    This expects a requirements file with no ``--find-links`` lines.
+    """
+    lines = requirements_file.read_text().strip().split('\n')
+    return [line for line in lines if not line.startswith('#')]
 
 
 def _get_class_name(homebrew_recipe_filename: str) -> str:
     """
     The Ruby class name depends on the file name.
+
     The first character is capitalized.
     Some characters are removed, and if a character is removed, the next
     character is capitalized.
+
     Returns:
         The Ruby class name to use, given a file name.
     """
@@ -30,34 +47,43 @@ def get_homebrew_formula(
     archive_url: str,
     head_url: str,
     homebrew_recipe_filename: str,
-    description: str,
-    homepage: str,
-    package_name: str,
 ) -> str:
     """
     Return the contents of a Homebrew formula for the CLIs.
     """
+    parent = Path(__file__).parent
+    repository_root = parent.parent
+    requirements = _get_dependencies(
+        requirements_file=repository_root / 'requirements.txt',
+    )
+
+    first = requirements[0]
+
+    # This command runs on a host which we do not control the OS of.
+    # We want our recipe to install on Homebrew (macOS) and Linuxbrew (Linux).
+    # Some packages have OS-specific dependencies.
+    # Our recipe will include the dependencies for our requirements for the OS
+    # that this command runs on.
+    # Some of those dependencies may error if installed on the "wrong"
+    # platform.
+    args = ['poet', first]
+    for requirement in requirements[1:]:
+        args.append('--also')
+        args.append(requirement)
+
+    result = subprocess.run(args=args, stdout=subprocess.PIPE, check=True)
+    resource_stanzas = str(result.stdout.decode())
+    homepage_url = ''
+
     class_name = _get_class_name(
         homebrew_recipe_filename=homebrew_recipe_filename,
     )
-
-    args = [
-        'pybrew',
-        '--formula-name',
-        class_name,
-        '--description',
-        description,
-        '--homepage',
-        homepage,
-        '--git-repo',
-        head_url,
-        '--release-url',
-        archive_url,
-        '--verbose',
-        package_name,
-        homebrew_recipe_filename,
-    ]
-
-    result = subprocess.run(args=args, stdout=subprocess.PIPE, check=True)
-    recipe = str(result.stdout.decode())
-    return recipe
+    homebrew_template_file = parent / 'homebrew_template.rb'
+    homebrew_template = homebrew_template_file.read_text()
+    return homebrew_template.format(
+        class_name=class_name,
+        resource_stanzas=resource_stanzas,
+        archive_url=archive_url,
+        head_url=head_url,
+        homepage_url=homepage_url,
+    )
---
+++ tests/test_admin/test_brew.py
@@ -0,0 +1,112 @@
+"""
+Tests for Homebrew and Linuxbrew.
+"""
+
+import logging
+import subprocess
+from pathlib import Path
+
+import docker
+from docker.types import Mount
+from dulwich.repo import Repo
+
+from admin.homebrew import get_homebrew_formula
+
+LOGGER = logging.getLogger(__name__)
+
+
+def test_brew(tmp_path: Path) -> None:
+    """
+    It is possible to create a Homebrew formula and to install this with
+    Linuxbrew.
+    """
+    # Homebrew requires the archive name to look like a valid version.
+    version = '1'
+    archive_name = '{version}.tar.gz'.format(version=version)
+    local_repository = Repo('.')
+    archive_file = tmp_path / archive_name
+    archive_file.touch()
+    # We do not use ``dulwich.porcelain.archive`` because it has no option to
+    # use a gzip format.
+    args = [
+        'git',
+        'archive',
+        '--format',
+        'tar.gz',
+        '-o',
+        str(archive_file),
+        '--prefix',
+        '{version}/'.format(version=version),
+        'HEAD',
+    ]
+    subprocess.run(args=args, check=True)
+
+    client = docker.from_env(version='auto')
+    linuxbrew_image = 'linuxbrew/linuxbrew'
+    # The path needs to look like a versioned artifact to Linuxbrew.
+    container_archive_path = '/' + archive_name
+    archive_url = 'file://' + container_archive_path
+    head_url = 'file://' + str(Path(local_repository.path).absolute())
+    homebrew_filename = 'vws.rb'
+
+    homebrew_formula_contents = get_homebrew_formula(
+        archive_url=archive_url,
+        head_url=head_url,
+        homebrew_recipe_filename=homebrew_filename,
+        # description='Some description',
+        # homepage='https://vws-cli.readthedocs.io',
+        # package_name='VWS-CLI',
+    )
+
+    homebrew_file = tmp_path / homebrew_filename
+    homebrew_file.write_text(homebrew_formula_contents)
+    container_homebrew_file_path = '/' + homebrew_filename
+
+    archive_mount = Mount(
+        source=str(archive_file.resolve().absolute()),
+        target=container_archive_path,
+        type='bind',
+    )
+
+    homebrew_file_mount = Mount(
+        source=str(homebrew_file.resolve().absolute()),
+        target=container_homebrew_file_path,
+        type='bind',
+    )
+
+    mounts = [archive_mount, homebrew_file_mount]
+    client.images.pull(repository=linuxbrew_image, tag='latest')
+    # Locally it is useful to run ``brew install`` with ``-v`` to expose
+    # issues.
+    # However, this produces a log which is too long for Travis CI.
+    #
+    # We see
+    # "The job exceeded the maximum log length, and has been terminated.".
+    command_list = [
+        'brew',
+        'install',
+        container_homebrew_file_path,
+        '&&',
+        'vws',
+        '--version',
+    ]
+
+    command = '/bin/bash -c "{command}"'.format(
+        command=' '.join(command_list),
+    )
+
+    container = client.containers.create(
+        image=linuxbrew_image,
+        mounts=mounts,
+        command=command,
+        environment={'HOMEBREW_NO_AUTO_UPDATE': 1},
+    )
+
+    container.start()
+    for line in container.logs(stream=True):
+        line = line.decode().strip()
+        LOGGER.info(line)
+
+    status_code = container.wait()['StatusCode']
+    assert status_code == 0
+    container.remove(force=True)
