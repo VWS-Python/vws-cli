fc10ed28b64fe5ee45731c9115c4a683f340bf83
Adam Dangoor <adamdangoor@gmail.com>
2019-12-29 19:02:38 +0000

    Add pyinstaller requirement

--- setup.py
+++ setup.py
@@ -20,24 +20,17 @@ def _get_dependencies(requirements_file: Path) -> List[str]:
     return [line for line in lines if not line.startswith('#')]
 
 
-_DIRECT_REQUIRES = _get_dependencies(
+INSTALL_REQUIRES = _get_dependencies(
     requirements_file=Path('requirements.txt'),
 )
 
-INSTALL_REQUIRES = _DIRECT_REQUIRES
 DEV_REQUIRES = _get_dependencies(
     requirements_file=Path('dev-requirements.txt'),
-)
-PACKAGING_REQUIRES = _get_dependencies(
-    requirements_file=Path('packaging-requirements.txt'),
 )
 
 setup(
     version=versioneer.get_version(),  # type: ignore
     cmdclass=versioneer.get_cmdclass(),  # type: ignore
     install_requires=INSTALL_REQUIRES,
-    extras_require={
-        'dev': DEV_REQUIRES,
-        'packaging': PACKAGING_REQUIRES,
-    },
+    extras_require={'dev': DEV_REQUIRES},
 )
