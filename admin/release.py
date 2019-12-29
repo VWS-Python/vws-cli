944b9f3f0644985d58710479d409b8bb3454008f
Adam Dangoor <adamdangoor@gmail.com>
2019-12-29 00:35:34 +0000

    Show git status

--- admin/release.py
+++ admin/release.py
@@ -62,6 +62,7 @@ def build_and_upload_to_pypi() -> None:
     for args in (
         ['git', 'fetch', '--tags'],
         ['rm', '-rf', 'build'],
+        ['git', 'status'],
         ['python', 'setup.py', 'sdist', 'bdist_wheel'],
         ['twine', 'upload', '-r', 'pypi', 'dist/*'],
     ):
