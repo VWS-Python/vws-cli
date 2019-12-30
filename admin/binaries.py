d79ed1d773de8692f3d9ba4944c48411492702d9
Adam Dangoor <adamdangoor@gmail.com>
2019-12-30 20:56:53 +0000

    Try showing issues as warnings

--- admin/binaries.py
+++ admin/binaries.py
@@ -57,7 +57,7 @@ def make_linux_binaries(repo_root: Path) -> Set[Path]:
     )
     for line in container.logs(stream=True):
         line = line.strip()
-        LOGGER.info(line)
+        LOGGER.warn(line)
 
     status_code = container.wait()['StatusCode']
     assert status_code == 0
