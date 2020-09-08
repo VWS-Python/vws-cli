4cdba4ca03e6eaecacd2aafd4b9a65fdb6bd60e2
Adam Dangoor <adamdangoor@gmail.com>
2020-09-08 12:32:46 +0100

    Add some tests for new VWQ error handling

--- tests/test_query_errors.py
+++ tests/test_query_errors.py
@@ -8,7 +8,10 @@ from pathlib import Path
 from typing import List
 
 from click.testing import CliRunner
+from freezegun import freeze_time
+from mock_vws import MockVWS
 from mock_vws.database import VuforiaDatabase
+from mock_vws.states import States
 from vws import VWS
 
 from vws_cli.query import vuforia_cloud_reco
@@ -107,6 +110,119 @@ def test_match_processing(
     expected_stderr = (
         'Error: The given image matches a target which was recently added, '
         'updated or deleted and Vuforia returns an error in this case.\n'
+    )
+    assert result.stderr == expected_stderr
+    assert result.stdout == ''
+
+
+def test_bad_image(
+    mock_database: VuforiaDatabase,
+    tmp_path: Path,
+) -> None:
+    """
+    An error is given when Vuforia returns a ``BadImage`` error. For example,
+    when a corrupt image is uploaded.
+    """
+    new_file = tmp_path / uuid.uuid4().hex
+    new_file.write_bytes(data=b'Not an image')
+    runner = CliRunner(mix_stderr=False)
+    commands: List[str] = [
+        str(new_file),
+        '--client-access-key',
+        mock_database.client_access_key,
+        '--client-secret-key',
+        mock_database.client_secret_key,
+    ]
+    result = runner.invoke(
+        vuforia_cloud_reco,
+        commands,
+        catch_exceptions=False,
+    )
+    assert result.exit_code == 1
+    expected_stderr = (
+        'Error: The given image is corrupted or the format is not supported.\n'
+    )
+    assert result.stderr == expected_stderr
+    assert result.stdout == ''
+
+
+def test_inactive_project(
+    high_quality_image: io.BytesIO,
+    tmp_path: Path,
+) -> None:
+    """
+    An error is given if the project is inactive and the desired action cannot
+    be taken because of this.
+    """
+    new_file = tmp_path / uuid.uuid4().hex
+    image_data = high_quality_image.getvalue()
+    new_file.write_bytes(data=image_data)
+    database = VuforiaDatabase(state=States.PROJECT_INACTIVE)
+    with MockVWS() as mock:
+        mock.add_database(database=database)
+        runner = CliRunner(mix_stderr=False)
+        commands = [
+            str(new_file),
+            '--client-access-key',
+            database.client_access_key,
+            '--client-secret-key',
+            database.client_secret_key,
+        ]
+        result = runner.invoke(
+            vuforia_cloud_reco,
+            commands,
+            catch_exceptions=False,
+        )
+
+    assert result.exit_code == 1
+    expected_stderr = (
+        'Error: The project associated with the given keys is inactive.\n'
+    )
+    assert result.stderr == expected_stderr
+    assert result.stdout == ''
+
+
+def test_request_time_too_skewed(
+    high_quality_image: io.BytesIO,
+    mock_database: VuforiaDatabase,
+    tmp_path: Path,
+) -> None:
+    """
+    An error is given when the request time is more than 65 minutes different
+    from the server time.
+    """
+    runner = CliRunner(mix_stderr=False)
+    vwq_max_time_skew = 60 * 65
+    leeway = 10
+    time_difference_from_now = vwq_max_time_skew + leeway
+    new_file = tmp_path / uuid.uuid4().hex
+    image_data = high_quality_image.getvalue()
+    new_file.write_bytes(data=image_data)
+
+    # We use a custom tick because we expect the following:
+    #
+    # * At least one time check when creating the request
+    # * At least one time check when processing the request
+    #
+    # >= 1 ticks are acceptable.
+    with freeze_time(auto_tick_seconds=time_difference_from_now):
+        commands = [
+            str(new_file),
+            '--client-access-key',
+            mock_database.client_access_key,
+            '--client-secret-key',
+            mock_database.client_secret_key,
+        ]
+        result = runner.invoke(
+            vuforia_cloud_reco,
+            commands,
+            catch_exceptions=False,
+        )
+
+    expected_stderr = (
+        'Error: Vuforia reported that the time given with this request was '
+        'outside the expected range. '
+        'This may be because the system clock is out of sync.\n'
     )
     assert result.stderr == expected_stderr
     assert result.stdout == ''
