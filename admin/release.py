540a9722508c0a9ddb8fadeda0d0570e3320af71
Adam Dangoor <adamdangoor@gmail.com>
2021-12-13 04:09:03 +0000

    Try moving a bunch of the release process to GitHub actions

--- admin/release.py
+++ admin/release.py
@@ -2,36 +2,14 @@
 Release the next version.
 """
 
-import datetime
 import os
 import subprocess
 from pathlib import Path
 
 from binaries import make_linux_binaries  # pylint: disable=import-error
 from github import Github
-from github.ContentFile import ContentFile
 from github.GitRelease import GitRelease
-from github.Repository import Repository
 from homebrew import update_homebrew  # pylint: disable=import-error
-
-
-def get_version(github_repository: Repository) -> str:
-    """
-    Return the next version.
-    This is today’s date in the format ``YYYY.MM.DD.MICRO``.
-    ``MICRO`` refers to the number of releases created on this date,
-    starting from ``0``.
-    """
-    utc_now = datetime.datetime.utcnow()
-    date_format = '%Y.%m.%d'
-    date_str = utc_now.strftime(date_format)
-    tag_labels = [tag.name for tag in github_repository.get_tags()]
-    today_tag_labels = [
-        item for item in tag_labels if item.startswith(date_str)
-    ]
-    micro = int(len(today_tag_labels))
-    new_version = f'{date_str}.{micro}'
-    return new_version
 
 
 def add_binaries_to_github_release(github_release: GitRelease) -> None:
@@ -56,38 +34,12 @@ def add_binaries_to_github_release(github_release: GitRelease) -> None:
         )
 
 
-def update_changelog(version: str, github_repository: Repository) -> None:
-    """
-    Add a version title to the changelog.
-    """
-    changelog_path = Path('CHANGELOG.rst')
-    branch = 'master'
-    changelog_content_file = github_repository.get_contents(
-        path=str(changelog_path),
-        ref=branch,
-    )
-    # ``get_contents`` can return a ``ContentFile`` or a list of
-    # ``ContentFile``s.
-    assert isinstance(changelog_content_file, ContentFile)
-    changelog_bytes = changelog_content_file.decoded_content
-    changelog_contents = changelog_bytes.decode('utf-8')
-    new_changelog_contents = changelog_contents.replace(
-        'Next\n----',
-        f'Next\n----\n\n{version}\n------------',
-    )
-    github_repository.update_file(
-        path=str(changelog_path),
-        message=f'Update for release {version}',
-        content=new_changelog_contents,
-        sha=changelog_content_file.sha,
-    )
-
-
 def main() -> None:
     """
     Perform a release.
     """
     github_token = os.environ['GITHUB_TOKEN']
+    version_str = os.environ['NEXT_VERSION']
     homebrew_tap_github_token = os.environ['HOMEBREW_TAP_GITHUB_TOKEN']
     github_repository_name = os.environ['GITHUB_REPOSITORY']
     github_client = Github(github_token)
@@ -95,8 +47,6 @@ def main() -> None:
     github_repository = github_client.get_repo(
         full_name_or_id=github_repository_name,
     )
-    version_str = get_version(github_repository=github_repository)
-    update_changelog(version=version_str, github_repository=github_repository)
     update_homebrew(
         homebrew_filename='vws-cli.rb',
         version_str=version_str,
@@ -105,15 +55,7 @@ def main() -> None:
             full_name_or_id='VWS-Python/homebrew-vws',
         ),
     )
-    github_release = github_repository.create_git_tag_and_release(
-        tag=version_str,
-        tag_message='Release ' + version_str,
-        release_name='Release ' + version_str,
-        release_message='See CHANGELOG.rst',
-        type='commit',
-        object=github_repository.get_commits()[0].sha,
-    )
-
+    github_release = github_repository.get_latest_release()
     add_binaries_to_github_release(github_release=github_release)
 
 
