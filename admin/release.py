889b17ad2febe9a0ad085fb74287c352079240e2
Adam Dangoor <adamdangoor@gmail.com>
2021-12-15 02:48:12 +0000

    Fix some lint issues

--- admin/release.py
+++ admin/release.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 from binaries import make_linux_binaries  # pylint: disable=import-error
 from github import Github
-from homebrew import update_homebrew  # pylint: disable=import-error
+from homebrew import get_homebrew_formula  # pylint: disable=import-error
 
 
 def main() -> None:
@@ -15,15 +15,10 @@ def main() -> None:
     Perform a release.
     """
     github_token = os.environ['GITHUB_TOKEN']
-    homebrew_tap_github_token = os.environ['HOMEBREW_TAP_GITHUB_TOKEN']
     github_repository_name = os.environ['GITHUB_REPOSITORY']
     github_client = Github(github_token)
-    homebrew_tap_github_client = Github(homebrew_tap_github_token)
     github_repository = github_client.get_repo(
         full_name_or_id=github_repository_name,
-    )
-    homebrew_tap_github_repository = homebrew_tap_github_client.get_repo(
-        full_name_or_id='VWS-Python/homebrew-vws',
     )
     homebrew_recipe_filename = 'vws-cli.rb'
     version_str = os.environ['NEXT_VERSION']
